4bc17baf5585805e957c1fd26e047b1dff465568
ToodlyDave <alapatdavid@gmail.com>
2021-12-02 16:31:00 +0530

    TEST all email cases

--- src/com/user/UserDetails.java
+++ src/com/user/UserDetails.java
@@ -4,9 +4,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserDetails {
-	
+
 	final String NAME = "^[A-Z][a-z]{2,}(\\s)?[A-Z][a-z]{2,}$";
-	final String EMAIL = "^[\\w-]+(\\.[\\w-]+)*@[^_\\W]+(\\.[^_\\W]+)?(?=(\\.[^_\\W]{3,}$|\\.[a-zA-Z]{2}$)).*$";
+	final String EMAIL = "^[\\w+-]+(\\.[\\w-]+)*@[^_\\W]+(\\.[^_\\W]+)?(?=(\\.[^_\\W]{3,}$|\\.[a-zA-Z]{2}$)).*$";
 	final String PHONE = "^[0-9]{1,3}[\\s][0-9]{10}$";
 	final String PASSWORD = "^(?=.*[A-Z])(?=.*[0-9])(?=[^_\\W]*[_\\W][^_\\W]*$).{8,}$";
 	
